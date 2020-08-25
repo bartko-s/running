a0e7cbf624a94be6754bc78fb3223cc7618f82a3
Bartko Stefan <bartko.stefan@gmail.com>
2020-08-25 20:50:32 +0200

    UPDATE - node 14

--- run.sh
+++ run.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 
 PS3='Please select action: '
-options=("development" "production" "production build" "quit")
+options=("development" "production" "production build" "connect to browser container" "connect to server container" "quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "development")
-            docker-compose -f ./docker-compose-production.yml -f ./docker-compose-development.yml build \
-            && docker-compose -f ./docker-compose-production.yml -f ./docker-compose-development.yml up \
-            && exit
+            docker-compose -f ./docker-compose-production.yml -f ./docker-compose-development.yml up --build
+            break
             ;;
         "production")
-            docker-compose -f ./docker-compose-production.yml build \
-            && docker-compose -f ./docker-compose-production.yml up \
-            && exit
+            docker-compose -f ./docker-compose-production.yml up --build
+            break
             ;;
         "production build")
-            docker-compose -f ./docker-compose-production.yml -f ./docker-compose-build.yml build \
-            && docker-compose -f ./docker-compose-production.yml -f ./docker-compose-build.yml up \
-            && exit
+            docker-compose -f ./docker-compose-production.yml -f ./docker-compose-build.yml up --build
+            break
+            ;;
+        "connect to browser container")
+            docker-compose -f ./docker-compose-production.yml exec node-browser bash
+            break
+            ;;
+        "connect to server container")
+            docker-compose -f ./docker-compose-production.yml exec node-server bash
+            break
             ;;
         "quit")
             break
