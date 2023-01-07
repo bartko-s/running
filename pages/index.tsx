02f5ebd5228c8145ff4e7913e3b31bd0da6b8991
Bartko Stefan <bartko.stefan@gmail.com>
2023-01-07 17:09:34 +0100

    CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -130,7 +130,7 @@ export default function Pace() {
   return (
       <>
         <Head>
-          <title>Running Calculator</title>
+          <title>Running Pace Calculator</title>
         </Head>
         <main className="content">
           {renderWorldRecordsModal()}
